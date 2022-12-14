50e04aebdfba856c5a311b31b2174cb8242ecc89
Franko777 <khubul.george@inbox.ru>
2022-12-14 11:34:07 +0300

    buildArray

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,27 @@
-
-
 fun main(args: Array<String>) {
-    concatenation(intArrayOf(10,1,2,3,5,6,7)).forEach { print(it)
+    concatenation(intArrayOf(10, 1, 2, 3, 5, 6, 7)).forEach {
+        print(it)
     }
+    buildArray(intArrayOf(0, 2, 1, 5, 3, 4)).forEach { print(" " + it) }
 }
 
-fun concatenation(nums: IntArray): IntArray{
-    val ans = IntArray(nums.size*2).apply {
-        for (i in 0..nums.size-1){
+fun concatenation(nums: IntArray): IntArray {
+    val ans = IntArray(nums.size * 2).apply {
+        for (i in 0..nums.size - 1) {
             this[i] = nums[i]
-            this[nums.size+i]=nums[i]
+            this[nums.size + i] = nums[i]
         }
     }
     return ans
 }
+
+fun buildArray(nums: IntArray): IntArray {
+    val ans = IntArray(nums.size).apply {
+        for (i in 0..nums.size - 1) {
+            if (nums[i] < nums.size) this[i] = nums[nums[i]]
+            else println("Error: nums[i]>=nums.size")
+        }
+    }
+    return ans
+}
+
