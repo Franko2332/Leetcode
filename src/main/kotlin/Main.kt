8e4d6365715ebf4343f4714cc0514e6c6c906da9
Franko777 <khubul.george@inbox.ru>
2022-12-14 10:55:00 +0300

    ex.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
+
+
 fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Program arguments: ${args.joinToString()}")
+    concatenation(intArrayOf(10,1,2,3,5,6,7)).forEach { print(it)
+    }
+}
+
+fun concatenation(nums: IntArray): IntArray{
+    val ans = IntArray(nums.size*2).apply {
+        for (i in 0..nums.size-1){
+            this[i] = nums[i]
+            this[nums.size+i]=nums[i]
+        }
+    }
+    return ans
 }
